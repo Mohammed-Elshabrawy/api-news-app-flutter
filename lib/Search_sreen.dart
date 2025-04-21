ad7f839dfedf449ce3cce96af4b10f1a960bf6cc
Mohammed-Elshabrawy <m.e18520029@gmail.com>
2025-04-21 15:20:31 +0200

    search done

--- lib/Search_sreen.dart
+++ lib/Search_sreen.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
+import 'details.dart';
 
 class SearchScreen extends StatelessWidget {
     const SearchScreen({super.key,required this.keyWord});
@@ -19,7 +19,9 @@ class SearchScreen extends StatelessWidget {
         body: FutureBuilder(
           future: getSearch(keyWord),
           builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-            if (snapshot.hasData) {
+            if(keyWord==" "){
+              return const Center(child: Text("Search Something"),);
+            } else if (snapshot.hasData) {
               if(snapshot.data['status']=="error"){
                 return  Padding(
                   padding: const EdgeInsets.all(8.0),
@@ -35,10 +37,13 @@ class SearchScreen extends StatelessWidget {
                         padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
                         child: InkWell(
                           onTap: () {
-                            String author , title , url , urlToImage,publishedAt;
-                            author=title=url=urlToImage=publishedAt="none";
+                            String author , title ,description, url ,urlToImage,publishedAt,content;
+                            author=title=url=urlToImage=publishedAt=content=description="none";
                             if(snapshot.data['articles'][i]['author']!=null){
                               author = snapshot.data['articles'][i]['author'];
+                            }
+                            if(snapshot.data['articles'][i]['description']!=null){
+                              description = snapshot.data['articles'][i]['description'];
                             }
                             if(snapshot.data['articles'][i]['title']!=null){
                               title = snapshot.data['articles'][i]['title'];
@@ -52,8 +57,11 @@ class SearchScreen extends StatelessWidget {
                             if(snapshot.data['articles'][i]['publishedAt']!=null){
                               publishedAt = snapshot.data['articles'][i]['publishedAt'];
                             }
-                            /* Navigator.pushReplacement(context,
-                                  MaterialPageRoute(builder: (context) => Details(author:author,title:title,url:url,publishedAt:publishedAt,urlToImage:urlToImage),));*/
+                            if(snapshot.data['articles'][i]['content']!=null){
+                              content = snapshot.data['articles'][i]['content'];
+                            }
+                            Navigator.pushReplacement(context,
+                                MaterialPageRoute(builder: (context) => Details(author:author,title:title,url:url,publishedAt:publishedAt,urlToImage:urlToImage, description: description, content: content,),));
                           },
                           child: Container(
                             height: 150,
@@ -80,4 +88,3 @@ class SearchScreen extends StatelessWidget {
     );
   }
 }
-
--- lib/details.dart
+++ lib/details.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:readmore/readmore.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'home_page.dart';
 
 class Details extends StatelessWidget {
   final String author;
   final String title;
+  final String description;
   final String url;
   final String publishedAt;
   final String urlToImage;
+  final String content;
   const Details(
       {super.key,  required this.author,
        required this.title,
        required this.url,
        required this.publishedAt,
-       required this.urlToImage});
+       required this.urlToImage,
+        required this.description,
+        required this.content});
 
   @override
   Widget build(BuildContext context) {
@@ -36,18 +41,17 @@ class Details extends StatelessWidget {
                     decoration: BoxDecoration(
                         color: Colors.cyan,
                         borderRadius: BorderRadius.circular(20)),
-                    child:   Padding(
+                    child:Padding(
                           padding: const EdgeInsets.all(20),
                           child: Column(
+                            mainAxisSize: MainAxisSize.max,
                                 children: [
                                   Text(
-                                    "$title read more",
+                                    "$title ",
                                     style: const TextStyle(
                                       fontSize: 18,
                                       color: Colors.white,
                                     ),
-                                    maxLines: 2,
-                                    overflow: TextOverflow.visible,
                                   ),
                                   const SizedBox(
                                     height: 10,
@@ -59,27 +63,51 @@ class Details extends StatelessWidget {
                                   const SizedBox(
                                     height: 10,
                                   ),
-                                  Row(
+                                  ReadMoreText(
+                                    description,trimMode: TrimMode.Line,
+                                    trimLines: 2,
+                                    colorClickableText: Colors.pink,
+                                    trimCollapsedText: 'Show more',
+                                    trimExpandedText: 'Show less',
+                                    // moreStyle: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
+                                    style:  const TextStyle(
+                                      fontSize: 18,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  const SizedBox(
+                                    height: 10,
+                                  ),
+                                  Stack(
                                     children: [
-                                      SizedBox(
-                                        width: 200,
-                                        child: Text(
-                                          "By $author",
-                                          maxLines: 1,
-                                          overflow: TextOverflow.ellipsis,
-                                          style: const TextStyle(
-                                            fontSize: 18,
-                                            color: Colors.white,
-                                          ),
-                                          textAlign: TextAlign.right,
+                                      Text(
+                                        description,
+                                        maxLines: 4,
+                                        overflow: TextOverflow.ellipsis,
+                                        style: const TextStyle(
+                                          fontSize: 18,
+                                          color: Colors.white,
                                         ),
+                                        textAlign: TextAlign.left,
                                       ),
-                                      TextButton(onPressed: () async{
-                                        if(url!="none"){
-                                          launchUrl(Uri.parse(url));
-                                        }
-                                      }, child: const Text("read more..",style: TextStyle(color: Colors.red),))
-                                    ],
+                                      Positioned(
+                                        right: 0,
+                                        bottom: 0,
+                                        child: TextButton(
+                                          style:  ButtonStyle(
+                                            minimumSize: const WidgetStatePropertyAll(Size(65, 15)),
+                                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                                            padding: const WidgetStatePropertyAll(EdgeInsets.zero),
+                                            backgroundColor: const WidgetStatePropertyAll(Colors.white),
+                                            shape: WidgetStatePropertyAll(RoundedRectangleBorder(borderRadius: BorderRadius.circular(100))),
+                                          ),
+                                            onPressed: () async{
+                                          if(url!="none"){
+                                            launchUrl(Uri.parse(url));
+                                          }
+                                        }, child: const Text("read more..",style: TextStyle(color: Colors.red,height: 1.5),)),
+                                      )
+                                    ]
                                   ),
                                   const SizedBox(
                                     height: 10,
@@ -87,13 +115,31 @@ class Details extends StatelessWidget {
                                   Row(
                                     children: [
                                       const Spacer(),
-                                      Text(
-                                        publishedAt,
-                                        style: const TextStyle(
-                                          fontSize: 12,
-                                          color: Colors.white,
-                                        ),
-                                        textAlign: TextAlign.right,
+                                      Column(
+                                        crossAxisAlignment: CrossAxisAlignment.end,
+                                        children: [
+                                          SizedBox(
+                                            width: 200,
+                                            child: Text(
+                                              "By $author",
+                                              maxLines: 1,
+                                              overflow: TextOverflow.ellipsis,
+                                              style: const TextStyle(
+                                                fontSize: 18,
+                                                color: Colors.white,
+                                              ),
+                                              textAlign: TextAlign.right,
+                                            ),
+                                          ),
+                                          Text(
+                                            publishedAt,
+                                            style: const TextStyle(
+                                              fontSize: 12,
+                                              color: Colors.white,
+                                            ),
+                                            textAlign: TextAlign.left,
+                                          ),
+                                        ],
                                       ),
                                     ],
                                   ),
